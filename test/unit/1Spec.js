4ef4a24d2aaa551f80b4ead5dc1a9c7ac29c8137
jesus_rp <rpsoft@gmail.com>
2015-02-23 11:54:08 +0000

    Added cursor pointer to showRoomImage

--- test/unit/1Spec.js
+++ test/unit/1Spec.js
@@ -6,7 +6,6 @@ describe('myApp', function() {
   var $controller;
 
   beforeEach(inject(function(_$controller_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
     $controller = _$controller_;
   }));
 
